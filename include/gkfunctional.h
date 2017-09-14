6652a271de6a63327ade19c213f1f46c0e07e599
makimoto <tkymakimoto@gmail.com>
2017-09-14 22:35:48 +0900

    1: Auto stash before merge of "develop_to_ver1" and "origin/develop_to_ver1"

--- include/gkfunctional.h
+++ include/gkfunctional.h
@@ -10,103 +10,106 @@
 
 namespace gk {
 
-template<typename T1, typename T2>
+template<typename _T1, typename _T2>
 struct product_type_definition {
 	typedef void value_type;
 };
 
 #if __cplusplus >= 201103L
 
-template<typename T1, typename T2>
-using product=typename product_type_definition<T1,T2>::value_type;
+/**
+ * @tparam __T1
+ */
+template<typename _T1, typename _T2>
+using product=typename product_type_definition<_T1,_T2>::value_type;
 #else
 #endif
 
-template<typename T1, typename T2>
+template<typename _T1, typename _T2>
 struct quotient_type_definition {
 	typedef void value_type;
 };
 
 #if __cplusplus >= 201103L
-template<typename T1, typename T2>
-using quotient = typename quotient_type_definition<T1,T2>::value_type;
+template<typename _T1, typename _T2>
+using quotient = typename quotient_type_definition<_T1,_T2>::value_type;
 #endif
 
 #if __cplusplus >= 201103L
-template<typename T1, typename T2>
-product<T1, T2> multiply(const T1& x, const T2& y) {
+template<typename _T1, typename _T2>
+product<_T1, _T2> multiply(const _T1& x, const _T2& y) {
 	return x * y;
 }
 
-template<typename T1, typename T2>
-product<T1, T2> operator*(const T1& x, const T2& y) {
+template<typename _T1, typename _T2>
+product<_T1, _T2> operator*(const _T1& x, const _T2& y) {
 	return x * y;
 }
 
-template<typename T1, typename T2, typename Result = product<T1, T2> >
+template<typename _T1, typename _T2, typename Result = product<_T1, _T2> >
 struct multiplier {
-	typedef T1 first_argument_type;
-	typedef T2 second_argument_type;
+	typedef _T1 first_argument_type;
+	typedef _T2 second_argument_type;
 	typedef Result result_type;
 
-	Result operator()(const T1& x, const T2& y) const {
+	Result operator()(const _T1& x, const _T2& y) const {
 		return multiply(x, y);
 	}
 };
 
 #else
 /**
- * @brief A multiplication function object to multiply a @a T1 type
- * by a @a T2 type.
+ * @brief A multiplication function object to multiply a @a _T1 type
+ * by a @a _T2 type.
  *
- * @tparam T1 First argument type.
- * @tparam T2 Second argument type.
+ * @tparam _T1 First argument type.
+ * @tparam _T2 Second argument type.
  * @tparam Result A result type to be computed.
  */
-template<typename T1, typename T2, typename Result = typename product_type_definition<
-T1, T2>::value_type> struct multiply {
-	typedef T1 first_argument_type;
-	typedef T2 second_argument_type;
+template<typename _T1, typename _T2, typename Result = typename product_type_definition<
+_T1, _T2>::value_type> struct multiply {
+	typedef _T1 first_argument_type;
+	typedef _T2 second_argument_type;
 	typedef Result result_type;
 
 	/**
 	 * @brief Multiplies @a x by @a y.
-	 * @param x The value of @c T1.
-	 * @param y The value of @c T2.
+	 * @param x The value of @c _T1.
+	 * @param y The value of @c _T2.
 	 * @return
 	 */
-	Result operator()(const T1& x, const T2& y) const {
+	Result operator()(const _T1& x, const _T2& y) const {
 		return x * y;
 	}
 };
 #endif
 
 #if __cplusplus >= 201103L
-template<typename T1, typename T2>
-quotient<T1, T2> devide(const T1& x, const T2& y) {
+template<typename _T1, typename _T2>
+quotient<_T1, _T2> devide(const _T1& x, const _T2& y) {
 	return x / y;
 }
 
-template<typename T1, typename T2>
-quotient<T1, T2> operator/(const T1& x, const T2& y) {
+template<typename _T1, typename _T2>
+quotient<_T1, _T2> operator/(const _T1& x, const _T2& y) {
 	return x / y;
 }
 
 #else
 /**
- * @brief A division function object to divide a @a T1 type by a @a T2 type.
+ * @brief A division function object to divide a @a _T1 type by a @a _T2 type.
  *
- * @tparam T1 First argument type.
- * @tparam T2 Second argument type.
+ * @tparam _T1 First argument type.
+ * @tparam _T2 Second argument type.
  * @tparam Result A result type to be computed.
  */
-template<typename T1, typename T2, typename Result = typename quotient_type_definition<T1,
-T2>::value_type> struct divides {
-	typedef T1 first_argument_type;
-	typedef T2 second_argument_type;
+template<typename _T1, typename _T2, typename Result = typename quotient_type_definition<_T1,
+_T2>::value_type> struct divides {
+	typedef _T1 first_argument_type;
+	typedef _T2 second_argument_type;
 	typedef Result result_type;
 
-	Result operator()(const T1& x, const T2& y) const {
+	Result operator()(const _T1& x, const _T2& y) const {
 		return x / y;
 	}
 };
